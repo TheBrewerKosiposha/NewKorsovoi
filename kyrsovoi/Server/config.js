f80e6b53b0fd269a16198fcedccd5356e0501f9b
TheBrewerKosiposha <fedya.makritsa@gmail.com>
2023-04-29 11:12:29 +0300

    first commit

---
+++ kyrsovoi/Server/config.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql2');
+
+const connectionPool = mysql.createPool({
+    connectionLimit: 10,
+    host: '10.208.125.86',
+    user: 'root',
+    password: '1234567890',
+    database: 'bd',
+})
+
+module.exports = connectionPool;
+
+//192.168.0.118;192.168.1.66
